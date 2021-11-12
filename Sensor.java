71d1aa2c40e30332f92c87daffeb91dbb9463a26
Fareed Osman <51924553+fkosman@users.noreply.github.com>
2021-11-12 13:21:48 -0700

    Draft of sensor component

---
+++ Sensor.java
@@ -0,0 +1,75 @@
+package Component.SISO;
+
+import model.modeling.content;
+
+public class Sensor extends siso {
+	public entity input;
+	
+	public double spacecraft_angle;
+	public double spacecraft_rot_speed;
+	public double wheel_tor;
+	public double external_tor;
+
+	private static final double time_step=0.000001;
+	private static final double init_angle=0;
+	private static final double spacecraft_inertia=10;
+	
+	public Sensor(){
+	    super("Sensor");
+		addInport("MotorPort");
+		addInport("ExternalPort");
+		addOutport("ControllerPort");
+		
+		addTestInput("ExternalPort", new doubleEnt((double)1));
+		addTestInput("ExternalPort", new doubleEnt((double)-1));
+		
+		initalize();
+	}
+	
+	public void initalize(){
+		spacecraft_angle = init_angle;
+		spacecraft_rot_speed = 0;
+		wheel_tor = 0;
+		external_tor = 0;
+		holdIn("Active", 1);
+		super.initialize();
+	}
+	
+	public void  deltext(double e, message x){
+		for(int i=0; i<x.getLength(); i++){
+			if (messageOnPort(x,"ExternalPort",i)){
+				input = x.getValOnPort("ExternalPort", i);
+				external_tor = Double.parseDouble(input.toString());
+			}
+			else if (messageOnPort(x,"MotorPort",i)){
+				input = x.getValOnPort("MotorPort", i);
+				wheel_tor = Double.parseDouble(input.toString());
+			}
+		}
+		Continue(e);
+	}
+	
+	public void deltint(){
+		double net_tor = external_tor - wheel_tor;
+		spacecraft_rot_speed += (net_tor / spacecraft_inertia);	
+		spacecraft_angle += (spacecraft_rot_speed * time_step);
+				
+		holdIn("Active", 1);
+	}
+	
+	public message out(){
+		message m = new message();
+		content con = makeContent("ControllerPort", new doubleEnt(spacecraft_angle));
+		m.add(con);
+		return m;
+	}
+	
+	public void showState(){
+		super.showState();
+	}
+	
+	public String getTooltipText(){
+		   return
+		   super.getTooltipText();
+		  }
+}
